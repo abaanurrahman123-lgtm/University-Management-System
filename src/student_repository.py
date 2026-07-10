b5af9cc639e7f208acfc76682c8b51bcaacf6e03
Abaan <abaanurrahman123@gmail.com>
2026-07-10 19:43:37 +0300

    Implement student repository lookup and update methods

--- src/student_repository.py
+++ src/student_repository.py
@@ -22,3 +22,27 @@ def save_all_students(students):
             for student in students
         ]
     )
+
+def get_student_by_tp(tp_number):
+    """
+    Return one Student object by TP number.
+    """
+    for student in get_all_students():
+        if student.tp_number == tp_number:
+            return student
+
+    return None
+
+def save_student(updated_student):
+    """
+    Update one student and save.
+    """
+
+    students = get_all_students()
+
+    for i, student in enumerate(students):
+        if student.tp_number == updated_student.tp_number:
+            students[i] = updated_student
+            break
+
+    save_all_students(students)
